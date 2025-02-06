7abf604e3643478732ad0d71dba42c2b1196aa99
Diosdado <diosdaditopaz@gmail.com>
2025-02-06 18:25:11 +0100

    Add New WebAPI Project

---
+++ src/ProductsCatalog.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsCatalog.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //GET
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Products");
+        }
+
+        //POST
+
+        //PUT
+
+        //DELETE
+    }
+}
